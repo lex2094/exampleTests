c23fce249d37ea8655498da3c3e0d958acaaa71f
lex2094 <alesha1240@mail.ru>
2022-09-16 20:53:02 +0300

    Second commit

---
+++ src/test/java/test/test/exampleTest2.java
@@ -0,0 +1,12 @@
+package test.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class exampleTest2 {
+    @Test
+    void exampleTest2(){
+        Assertions.assertTrue(100>99);
+
+    }
+}
