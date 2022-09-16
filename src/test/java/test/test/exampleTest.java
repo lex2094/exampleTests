305865f6d95046d7fc4c24c61d9cdb5d147b5ba5
lex2094 <alesha1240@mail.ru>
2022-09-16 20:49:48 +0300

    first commit

---
+++ src/test/java/test/test/exampleTest.java
@@ -0,0 +1,12 @@
+package test.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class exampleTest {
+
+    @Test
+    void exampleTest(){
+        Assertions.assertTrue(2>1);
+    }
+}
